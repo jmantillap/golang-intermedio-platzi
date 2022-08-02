34744e75dbc1b576a0948274165e6103eaa0c3dd
Javier Orlando Mantilla Portilla <jmantilla@intergrupo.com>
2022-08-02 14:59:06 -0500

    Se agregan metodos getter and setters para go. Funciones receivers

--- class.go
+++ class.go
@@ -7,12 +7,36 @@ type Empleado struct {
 	name string
 }
 
+func (e *Empleado) SetId(id int) {
+	e.id = id
+}
+
+func (e *Empleado) SetName(name string) {
+	e.name = name
+}
+
+func (e *Empleado) GetId() int {
+	return e.id
+}
+
+func (e *Empleado) GetName() string {
+	return e.name
+}
+
 func main() {
 
 	e := Empleado{}
-	fmt.Printf("%v", e)
+	fmt.Printf("%v\n", e)
 
 	e.id = 1
 	e.name = "Name"
-	fmt.Printf("%v", e)
+	fmt.Printf("%v\n", e)
+
+	e.SetId(10)
+	e.SetName("Javier")
+	fmt.Printf("%v\n", e)
+
+	fmt.Println(e.GetId())
+	fmt.Println(e.GetName())
+
 }
