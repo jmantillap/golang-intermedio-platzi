c432c0448cb28beb96b255742f18b57f7c2f47d3
Javier Orlando Mantilla Portilla <jmantilla@intergrupo.com>
2022-08-02 14:07:58 -0500

    Repaso inicial

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"strconv"
+	"time"
 )
 
 func main() {
@@ -41,4 +42,20 @@ func main() {
 		fmt.Println(value)
 	}
 
+	c := make(chan int)
+	go doSomething(c)
+	<-c
+
+	g := 25
+	fmt.Println(g) // imprime el valor entero 25
+	h := &g
+	fmt.Println(h) // imprimer la direccion de memoria.
+	i := *h
+	fmt.Println(i) // Imprime el valor por de g
+}
+
+func doSomething(c chan int) {
+	time.Sleep(3 * time.Second)
+	fmt.Println("done")
+	c <- 1
 }
