a2d9f46aa88345bb451ec6bed2c278ea0badba5a
Javier Orlando Mantilla Portilla <jmantilla@intergrupo.com>
2022-08-02 13:42:15 -0500

    commint inicial

---
+++ main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+)
+
+func main() {
+
+	var x int
+	x = 8
+	y := 1
+
+	fmt.Println(x)
+	fmt.Println(y)
+	myValue, err := strconv.ParseInt("7ss", 0, 64)
+	if err != nil {
+		fmt.Printf("Err %v\n", err)
+	} else {
+		fmt.Println(myValue)
+	}
+	// Mapa: estructura de clave valor, con Make especificar un map que mapee llaves de tipo string a valores de tipo entero
+	m := make(map[string]int)
+	//Key= string, 6= int
+	m["key"] = 6
+	fmt.Println(m["key"])
+
+	// Slice: Estructura como un array
+	s := []int{1, 2, 3}
+	// Con el for recorremos el slice
+	for index, value := range s {
+		//index: valor en memoria que estamos accediendo
+		fmt.Println(index)
+		//value: valor almacenado en el slice
+		fmt.Println(value)
+	}
+	// Agregar un valor nuevo al final del slice
+	s = append(s, 16)
+	for index, value := range s {
+		fmt.Println(index)
+		fmt.Println(value)
+	}
+
+}
